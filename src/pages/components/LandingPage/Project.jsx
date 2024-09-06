af61bb03fd2648a000f0a1ed0d7f6f4ab6b4ebd6
Hendraghz <hendraghz79@gmail.com>
2024-09-06 21:33:23 +0700

    Perbaikan Halaman Landing page

--- src/pages/components/LandingPage/Project.jsx
+++ src/pages/components/LandingPage/Project.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { getProject } from "../../../api/project/apiproject";
 import { ApiUrl } from "../../../api/baseUrl";
+import asset from "../../../assets/images/Asset4.webp";
 
 const Project = () => {
   const [loading, setLoading] = useState(true);
@@ -30,106 +31,129 @@ const Project = () => {
       currency: "IDR",
     }).format(value);
   return (
-    <div className="bg-white px-4 md:px-10  ">
-      <div className="border-b border-color-1">
-        <p className="title font-bold text-2xl  text-color-1 py-2">
-          PROJECT TERBARU
+    <div className="flex flex-col items-center">
+      <img
+        src={asset}
+        alt="hijab"
+        className="w-full scale-x-[-1] h-[48rem] object-cover object-top"
+      />
+      <div className="absolute -ml-[50rem] mt-[20rem]">
+        <p className="text-white text-2xl font-bold">Our Projects</p>
+        <p className=" text-white text-5xl font-extrabold mt-2">
+          Explore investment <br /> opportunities handpicked for <br /> growth.
+          Join us in shaping <br /> tomorrow success stories.
         </p>
+        <button className="px-8 py-3 bg-color-2  font-bold mt-[1rem] hover:bg-white">Project</button>
       </div>
-      <div className="flex items-center gap-4 overflow-auto mt-[2rem]">
-        {loading ? (
-          Array.from({ length: 3 }).map((_, idx) => (
-            <div
-              key={idx}
-              className="w-[20rem] h-[30rem] bg-gray-100 animate-pulse rounded-md"
-            >
-              <div className="w-2/3 p-4 md:m-4">
-                <h1 className="w-40 h-2 bg-gray-200 rounded-lg dark:bg-gray-700"></h1>
-                <p className="w-48 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
-                <div className="flex mt-4 item-center gap-x-2">
-                  <p className="w-5 h-2 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
-                  <p className="w-5 h-2 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
-                  <p className="w-5 h-2 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
-                  <p className="w-5 h-2 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
-                  <p className="w-5 h-2 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
-                </div>
-                <div className="flex justify-between mt-6 item-center">
-                  <h1 className="w-10 h-2 bg-gray-200 rounded-lg dark:bg-gray-700"></h1>
-                  <div className="h-4 bg-gray-200 rounded-lg w-28 dark:bg-gray-700"></div>
-                </div>
-              </div>
-            </div>
-          ))
-        ) : project.length === 0 ? (
-          <div className="w-full text-center mt-[2rem]">
-            <p className="text-gray-500 text-xl">Tidak ada project tersedia.</p>
-          </div>
-        ) : (
-          project.map((pro) => {
-            const tercapaiNum = pro.tercapai;
-            const targetNum = pro.target;
-            const percentage = (tercapaiNum / targetNum) * 100;
-
-            return (
-              <div key={pro.id} className="pb-[1rem]">
-                <Link to={`/detail-project/${pro.id}`}>
-                  <div className="w-[20rem] h-[36rem] shadow-xl text-black rounded-md border bg-white">
-                    <div>
-                      <img
-                        src={`${ApiUrl}${pro.image}`}
-                        alt={pro.nama}
-                        className="rounded-md h-[15rem]"
-                        loading="lazy"
-                      />
-                    </div>
-                    <div className="tipe mt-[1rem] px-3">
-                      <p className="px-2 py-1 bg-color-1 text-white w-fit rounded-md uppercase text-xs">
-                        {pro.tipe}
-                      </p>
-                    </div>
-                    <div className="mt-[1rem] px-3 flex flex-col justify-between h-[16rem]">
-                      <div>
-                        <p className="text-base font-bold">
-                          {pro.nama} - {pro.oleh} | Tahun 2024
-                        </p>
-                        <p className="text-xs mt-1">{pro.desc}</p>
-                      </div>
-                      <div>
-                        <div className="flex justify-between items-center mt-[1rem]">
-                          <div className="tercapai text-left">
-                            <p className="text-xs mb-2">Tercapai</p>
-                            <p className="text-sm text-color-1 font-bold">
-                              {numberFormat(pro.tercapai)}
-                            </p>
-                          </div>
-                          <div className="tercapai text-right ">
-                            <p className="text-xs mb-2">Target</p>
-                            <p className="text-sm text-color-1">{numberFormat(pro.target)}</p>
-                          </div>
-                        </div>
-                        <div className="bg-gray-200 rounded-full h-2.5 mt-3">
-                          <div
-                            className="bg-color-1 h-2.5 rounded-full"
-                            style={{ width: `${percentage}%` }}
-                          ></div>
-                        </div>
-                        <div className="mt-3 flex items-center justify-between pb-[1rem]">
-                          <div className="sisawaktu">
-                            <p className="font-normal text-xs">Waktu Tersisa</p>
-                            <p className="text-base font-medium">{pro.waktu_tersisa}</p>
-                          </div>
-                          <button className="text-color-1 text-sm bg-color-2 px-2 py-2 rounded-md">
-                            Detail Project
-                          </button>
-                        </div>
-                      </div>
-                    </div>
+      <div className="bg-white px-4 md:px-10  md:w-full 2xl:w-4/5">
+        <div className="border-b border-color-1">
+          <p className="title font-bold text-2xl  text-color-1 py-2">
+            PROJECT TERBARU
+          </p>
+        </div>
+        <div className="flex items-center gap-4 overflow-auto mt-[2rem]">
+          {loading ? (
+            Array.from({ length: 3 }).map((_, idx) => (
+              <div
+                key={idx}
+                className="w-[20rem] h-[30rem] bg-gray-100 animate-pulse rounded-md"
+              >
+                <div className="w-2/3 p-4 md:m-4">
+                  <h1 className="w-40 h-2 bg-gray-200 rounded-lg dark:bg-gray-700"></h1>
+                  <p className="w-48 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
+                  <div className="flex mt-4 item-center gap-x-2">
+                    <p className="w-5 h-2 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
+                    <p className="w-5 h-2 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
+                    <p className="w-5 h-2 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
+                    <p className="w-5 h-2 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
+                    <p className="w-5 h-2 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
                   </div>
-                </Link>
+                  <div className="flex justify-between mt-6 item-center">
+                    <h1 className="w-10 h-2 bg-gray-200 rounded-lg dark:bg-gray-700"></h1>
+                    <div className="h-4 bg-gray-200 rounded-lg w-28 dark:bg-gray-700"></div>
+                  </div>
+                </div>
               </div>
-            );
-          })
-        )}
+            ))
+          ) : project.length === 0 ? (
+            <div className="w-full text-center mt-[2rem]">
+              <p className="text-gray-500 text-xl">
+                Tidak ada project tersedia.
+              </p>
+            </div>
+          ) : (
+            project.map((pro) => {
+              const tercapaiNum = pro.tercapai;
+              const targetNum = pro.target;
+              const percentage = (tercapaiNum / targetNum) * 100;
+
+              return (
+                <div key={pro.id} className="pb-[1rem]">
+                  <Link to={`/detail-project/${pro.id}`}>
+                    <div className="w-[20rem] h-[36rem] shadow-xl text-black rounded-md border bg-white">
+                      <div>
+                        <img
+                          src={`${ApiUrl}${pro.image}`}
+                          alt={pro.nama}
+                          className="rounded-md h-[15rem]"
+                          loading="lazy"
+                        />
+                      </div>
+                      <div className="tipe mt-[1rem] px-3">
+                        <p className="px-2 py-1 bg-color-1 text-white w-fit rounded-md uppercase text-xs">
+                          {pro.tipe}
+                        </p>
+                      </div>
+                      <div className="mt-[1rem] px-3 flex flex-col justify-between h-[16rem]">
+                        <div>
+                          <p className="text-base font-bold">
+                            {pro.nama} - {pro.oleh} | Tahun 2024
+                          </p>
+                          <p className="text-xs mt-1">{pro.desc}</p>
+                        </div>
+                        <div>
+                          <div className="flex justify-between items-center mt-[1rem]">
+                            <div className="tercapai text-left">
+                              <p className="text-xs mb-2">Tercapai</p>
+                              <p className="text-sm text-color-1 font-bold">
+                                {numberFormat(pro.tercapai)}
+                              </p>
+                            </div>
+                            <div className="tercapai text-right ">
+                              <p className="text-xs mb-2">Target</p>
+                              <p className="text-sm text-color-1">
+                                {numberFormat(pro.target)}
+                              </p>
+                            </div>
+                          </div>
+                          <div className="bg-gray-200 rounded-full h-2.5 mt-3">
+                            <div
+                              className="bg-color-1 h-2.5 rounded-full"
+                              style={{ width: `${percentage}%` }}
+                            ></div>
+                          </div>
+                          <div className="mt-3 flex items-center justify-between pb-[1rem]">
+                            <div className="sisawaktu">
+                              <p className="font-normal text-xs">
+                                Waktu Tersisa
+                              </p>
+                              <p className="text-base font-medium">
+                                {pro.waktu_tersisa}
+                              </p>
+                            </div>
+                            <button className="text-color-1 text-sm bg-color-2 px-2 py-2 rounded-md">
+                              Detail Project
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </Link>
+                </div>
+              );
+            })
+          )}
+        </div>
       </div>
     </div>
   );
--- src/pages/layout/Navbar.jsx
+++ src/pages/layout/Navbar.jsx
@@ -41,92 +41,96 @@ const Navbar = () => {
 
   return (
     <div
-      className={`fixed flex justify-between items-center top-0 left-0 w-full z-50 px-5 md:px-[5rem] ${
+      className={`fixed flex justify-between items-center top-0 left-0 w-full z-50 px-5 py-3 md:px-[5rem]  ${
         !scrolled ? "border-b border-color-1" : "shadow-md"
       } ${menuOpen ? "bg-white" : "md:bg-white"} bg-white md:bg-transparent`}
       style={
         window.innerWidth >= 768 ? navbarStyle : { backgroundColor: "#ffffff" }
       }
     >
-      <div className="flex items-center">
-        <Link to={"/"} className="block py-1">
-          <img
-            src={isLayananPage || scrolled || menuOpen ? LogoWhite : LogoWhite}
-            alt="logo-invsma"
-            height={isLayananPage || scrolled || menuOpen ? 20 : 20}
-            width={isLayananPage || scrolled || menuOpen ? 55 : 55}
-          />
-        </Link>
-      </div>
-      <div className="flex items-center md:hidden z-50">
-        <button
-          onClick={() => setMenuOpen(!menuOpen)}
-          className="text-black focus:outline-none"
-        >
-          {menuOpen ? (
-            <svg
-              className="w-6 h-6"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M6 18L18 6M6 6l12 12"
-              ></path>
-            </svg>
-          ) : (
-            <svg
-              className="w-6 h-6"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16M4 18h16"
-              ></path>
-            </svg>
-          )}
-        </button>
-      </div>
-      <nav
-        className={`fixed top-0 left-0 h-full w-full bg-white transform ${
-          menuOpen ? "translate-x-0" : "translate-x-full"
-        } transition-transform duration-300 ease-in-out md:static md:flex md:items-center md:justify-center md:bg-transparent md:transform-none z-40`}
-      >
-        <div className="flex flex-col items-center justify-center mt-20 md:mt-0 md:flex-row">
-          {navigation.map((item) => (
-            <Link
-              to={item.url}
-              key={item.id}
-              className={`relative block text-2xl group transition-colors ${
-                isLayananPage || scrolled || menuOpen
-                  ? "text-white"
-                  : "text-white"
-              } ${
-                item.onlyMobile ? "lg:hidden" : ""
-              } px-3 py-3 pb-2 md:py-4 lg:mb lg:-mr-0.25 lg:text-[1rem] lg:font-semibold lg:leading-5 xl:px-8`}
-              onClick={() => setMenuOpen(false)}
-            >
-              {item.title}
-              <span
-                className={`block ${
-                  location.pathname === item.url
-                    ? "h-0.5 bg-white mt-1 max-w-full"
-                    : "max-w-0"
-                } group-hover:max-w-full transition-all duration-500 h-0.5 bg-color-1 mt-1`}
-              ></span>
-            </Link>
-          ))}
+      <div className="flex items-center justify-between">
+        <div className="flex items-center">
+          <Link to={"/"} className="block py-1">
+            <img
+              src={
+                isLayananPage || scrolled || menuOpen ? LogoWhite : LogoWhite
+              }
+              alt="logo-invsma"
+              height={isLayananPage || scrolled || menuOpen ? 20 : 20}
+              width={isLayananPage || scrolled || menuOpen ? 55 : 55}
+            />
+          </Link>
         </div>
-      </nav>
+        <div className="flex items-center md:hidden z-50">
+          <button
+            onClick={() => setMenuOpen(!menuOpen)}
+            className="text-black focus:outline-none"
+          >
+            {menuOpen ? (
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                ></path>
+              </svg>
+            ) : (
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                ></path>
+              </svg>
+            )}
+          </button>
+        </div>
+        <nav
+          className={`fixed top-0 left-0 h-full w-full bg-white transform ${
+            menuOpen ? "translate-x-0" : "translate-x-full"
+          } transition-transform duration-300 ease-in-out md:static md:flex md:items-center md:justify-center md:bg-transparent md:transform-none z-40`}
+        >
+          <div className="flex flex-col items-center justify-center mt-20 md:mt-0 md:flex-row">
+            {navigation.map((item) => (
+              <Link
+                to={item.url}
+                key={item.id}
+                className={`relative block text-2xl group transition-colors ${
+                  isLayananPage || scrolled || menuOpen
+                    ? "text-white"
+                    : "text-white"
+                } ${
+                  item.onlyMobile ? "lg:hidden" : ""
+                } px-3 py-3 pb-2 md:py-4 lg:mb lg:-mr-0.25 lg:text-[1rem] lg:font-semibold lg:leading-5 xl:px-8`}
+                onClick={() => setMenuOpen(false)}
+              >
+                {item.title}
+                <span
+                  className={`block ${
+                    location.pathname === item.url
+                      ? "h-0.5 bg-white mt-1 max-w-full"
+                      : "max-w-0"
+                  } group-hover:max-w-full transition-all duration-500 h-0.5 bg-color-1 mt-1`}
+                ></span>
+              </Link>
+            ))}
+          </div>
+        </nav>
+      </div>
       <div className="hidden md:flex items-center px-7 gap-3">
         {user ? ( // Check if user is logged in
           <>
